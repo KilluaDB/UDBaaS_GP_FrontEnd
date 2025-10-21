72ff8838eb39e5d841bcc17ca54254258f7ebd93
Aya <ayahame99@gmail.com>
2025-10-21 18:56:42 +0300

    add screenUtil package for responsive design

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dbaas_project/core/app_theme.dart';
 import 'package:dbaas_project/features/Auth/presentation/screens/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(DBaasApp());
@@ -11,15 +12,20 @@ class DBaasApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        RegisterScreen.routeName:(_)=>RegisterScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(1440, 1024),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          routes: {RegisterScreen.routeName: (_) => RegisterScreen()},
+          initialRoute: RegisterScreen.routeName,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: ThemeMode.light,
+        );
       },
-      initialRoute: RegisterScreen.routeName,
-      theme:AppTheme.lightTheme ,
-      darkTheme:AppTheme.darkTheme ,
-      themeMode: ThemeMode.light,
     );
   }
 }
