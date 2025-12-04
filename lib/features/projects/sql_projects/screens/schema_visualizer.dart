8aadaecf84721f8bc181d69bcc2bf71829d3187e
Aya <ayahame99@gmail.com>
2025-12-04 03:42:02 +0200

    sql tabs UI

---
+++ lib/features/projects/sql_projects/screens/schema_visualizer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SchemaVisualizer extends StatelessWidget {
+  const SchemaVisualizer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Done');
+  }
+}
