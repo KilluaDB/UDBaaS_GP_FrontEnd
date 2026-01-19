65b7851bec2f1b0daba7833db0b50327e395638f
Aya <ayahame99@gmail.com>
2026-01-19 03:23:20 +0200

    view of Auth and add cubit to it

---
+++ lib/features/Auth/view/screens/register_screen.dart
@@ -0,0 +1,152 @@
+import 'package:dbaas_project/core/app_theme.dart';
+import 'package:dbaas_project/core/constants/app_images.dart';
+import 'package:dbaas_project/core/provider/user_provider.dart';
+import 'package:dbaas_project/features/Auth/view/widgets/auth_section.dart';
+import 'package:dbaas_project/features/Auth/view_model/auth_view_model.dart';
+import 'package:dbaas_project/l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+class RegisterScreen extends StatefulWidget {
+  static const String routeName = '/register';
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  bool isSignIn = true;
+  bool isSignUp = false;
+
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+    AppLocalizations local = AppLocalizations.of(context)!;
+  double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: AppTheme.white,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  AppImages.appIcon,
+                  fit: BoxFit.contain,
+                  width: 120.w,
+                  height: 120.h,
+                ),
+                SizedBox(height: 16.h),
+                Text(
+                  local.aiDbHub,
+                  style: textTheme.headlineLarge?.copyWith(fontSize: 28.sp),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 8.h),
+                Text(
+                  local.yourIntelligentDatabaseManagementSolution,
+                  style: textTheme.titleMedium?.copyWith(fontSize: 16.sp),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 32.h),
+
+          
+                Container(
+              width: screenWidth*(557/screenWidth).w,
+                  height: (642).h,
+                  padding: EdgeInsets.symmetric(horizontal: 34.w, vertical: 34.h),
+                  decoration: BoxDecoration(
+                    color: AppTheme.white,
+                    borderRadius: BorderRadius.circular(16.r),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 10.r,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      // Tabs
+                      Container(
+                        decoration: BoxDecoration(
+                          color: AppTheme.semiGray,
+                          borderRadius: BorderRadius.circular(14.r),
+                        ),
+                        child: Row(
+                          children: [
+                            buildTab(local.signIn, isSignIn, () {
+                              setState(() {
+                                isSignIn = true;
+                                isSignUp = false;
+                              });
+                            }),
+                            buildTab(local.signUp, isSignUp, () {
+                              setState(() {
+                                isSignUp = true;
+                                isSignIn = false;
+                              });
+                            }),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 24.h),
+
+                      // Auth Section
+                      BlocProvider(
+                        create: (_) => AuthViewModel(userProvider: context.read<UserProvider>()),
+                        child: AuthSection(
+                          isSignUp: isSignUp,
+                          onTap: switchAuthentication,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+            
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTab(String title, bool isSelected, VoidCallback onTap) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 4.w, vertical: 4.h),
+          padding: EdgeInsets.symmetric(vertical: 10.h),
+          decoration: BoxDecoration(
+            color: isSelected ? Colors.white : Colors.transparent,
+            borderRadius: BorderRadius.circular(10.r),
+          ),
+          child: Center(
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+                color: isSelected ? AppTheme.primary : Colors.black,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void switchAuthentication() {
+    setState(() {
+      isSignIn = !isSignIn;
+      isSignUp = !isSignUp;
+    });
+  }
+}
---
+++ lib/features/Auth/view/widgets/auth_section.dart
@@ -0,0 +1,177 @@
+import 'package:dbaas_project/core/app_theme.dart';
+import 'package:dbaas_project/core/constants/app_images.dart';
+import 'package:dbaas_project/core/util/ui_utils.dart';
+import 'package:dbaas_project/core/util/validator.dart';
+
+import 'package:dbaas_project/core/widgets/custome_elevated_button.dart';
+import 'package:dbaas_project/core/widgets/custome_text_form_field.dart';
+
+import 'package:dbaas_project/features/Auth/view_model/auth_states.dart';
+import 'package:dbaas_project/features/Auth/view_model/auth_view_model.dart';
+
+import 'package:dbaas_project/features/home/home_screen.dart';
+import 'package:dbaas_project/l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AuthSection extends StatefulWidget {
+  final bool isSignUp;
+  final VoidCallback onTap;
+  const AuthSection({super.key, required this.isSignUp, required this.onTap});
+
+  @override
+  State<AuthSection> createState() => _AuthSectionState();
+}
+
+class _AuthSectionState extends State<AuthSection> {
+  late TextEditingController emailController;
+  late TextEditingController passwordController;
+  late GlobalKey<FormState> formKey;
+
+  @override
+  void initState() {
+    super.initState();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    formKey = GlobalKey<FormState>();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+       AppLocalizations local = AppLocalizations.of(context)!;
+    TextTheme textTheme = Theme.of(context).textTheme;
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        Text(local.emailAddress, style: textTheme.titleSmall),
+          SizedBox(height: 8.h),
+          CustomTextFormField(
+            controller: emailController,
+            validator: (value) {
+              return Validator.validateEmail(value);
+            },
+            hintText: 'Enter your email',
+            prefixIconName: AppImages.emailIcon,
+          ),
+          SizedBox(height: 20.h),
+          Text(local.password, style: textTheme.titleSmall),
+          SizedBox(height: 8.h),
+          CustomTextFormField(
+            controller: passwordController,
+            validator: (value) {
+              return Validator.validatePassword(value);
+            },
+            hintText: 'Enter your password',
+            isPassword: true,
+            prefixIconName: AppImages.passwordIcon,
+          ),
+
+          SizedBox(height: 20.h),
+          widget.isSignUp == false
+              ? BlocListener<AuthViewModel, AuthState>(
+                  listener: (context, state) {
+                    if (state is LoginLoading) {
+                      UiUtils.showLoading(context);
+                    } else if (state is LoginSuccess) {
+                      UiUtils.hideLoading(context);
+                      UiUtils.showSuccessMessage("Successfull Login !");
+                      Navigator.of(
+                        context,
+                      ).pushReplacementNamed(HomeScreen.routeName);
+                    } else if (state is LoginError) {
+                      UiUtils.hideLoading(context);
+                      UiUtils.showErrorMessage(state.message);
+                    }
+                  },
+                  child: CustomElevatedButton(
+                    onTap: () {
+                      if (formKey.currentState!.validate()) {
+                        context.read<AuthViewModel>().login(
+                          emailController.text,
+                          passwordController.text,
+                        );
+                      }
+                    },
+                    child: Text(
+                     local.signIn,
+                      style: textTheme.titleSmall!.copyWith(
+                        color: AppTheme.white,
+                      ),
+                    ),
+                  ),
+                )
+              : BlocListener<AuthViewModel, AuthState>(
+                  listener: (context, state) {
+                    if (state is RegisterLoading) {
+                      UiUtils.showLoading(context);
+                    } else if (state is RegisterSuccess) {
+                      UiUtils.hideLoading(context);
+                      UiUtils.showSuccessMessage(
+                        state.registerResponse.message!,
+                      );
+                      Navigator.of(
+                        context,
+                      ).pushReplacementNamed(HomeScreen.routeName);
+                    } else if (state is RegisterError) {
+                      UiUtils.hideLoading(context);
+                      UiUtils.showErrorMessage(state.message);
+                    }
+                  },
+                  child: CustomElevatedButton(
+                    onTap: () {
+                      if (formKey.currentState!.validate()) {
+                        context.read<AuthViewModel>().register(
+                          emailController.text,
+                          passwordController.text,
+                        );
+                      }
+                    },
+                    child: Text(
+                    local.createAccount,
+                      style: textTheme.titleSmall!.copyWith(
+                        color: AppTheme.white,
+                      ),
+                    ),
+                  ),
+                ),
+
+          SizedBox(height: 24.h),
+          Divider(indent: 1, endIndent: 3),
+          SizedBox(height: 24.h),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                widget. isSignUp ? local.alreadyHaveAccount : local.dontHaveAccount,
+                style: textTheme.titleSmall,
+                  softWrap: true,
+              ),
+              InkWell(
+                onTap: widget.onTap,
+                child: Text(
+                    softWrap: true,
+                  widget. isSignUp ?local.signIn:local.signUp,
+                  style: textTheme.titleSmall!.copyWith(
+                    color: AppTheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+}
