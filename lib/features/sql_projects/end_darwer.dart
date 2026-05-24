697e2bb318d001388496f61a401b8f5f60b8b617
Aya <ayahame99@gmail.com>
2026-05-24 22:28:44 +0300

    different drawers in sql projects

---
+++ lib/features/sql_projects/end_darwer.dart
@@ -0,0 +1,24 @@
+import 'package:dbaas_project/features/projects/data/models/project_model.dart';
+import 'package:dbaas_project/features/sql_projects/DB/view/widgets/drawer_db.dart';
+import 'package:dbaas_project/features/sql_projects/schema_generation/view/widgets/drawer_chatbot.dart';
+import 'package:dbaas_project/features/sql_projects/table_editor/drawer_table.dart';
+import 'package:flutter/material.dart';
+
+class EndDarwer extends StatelessWidget {
+  int selectedIndex;
+  ProjectModel project;
+  EndDarwer({required this.selectedIndex,required this.project});
+
+  @override
+  Widget build(BuildContext context) {
+    switch (selectedIndex) {
+      case 1:
+        return DrawerDB(projectId: project.id!,);
+      case 2:
+        return DrawerTable();
+      case 5:
+        return DrawerChatbot();
+    }
+    return SizedBox();
+  }
+}
---
+++ lib/features/sql_projects/table_editor/drawer_table.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrawerTable extends StatelessWidget {
+  const DrawerTable({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
