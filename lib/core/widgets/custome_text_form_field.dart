b57f1ac7d5e35782ad168c0acd0fdf94596f5381
Aya <ayahame99@gmail.com>
2026-05-24 22:28:11 +0300

    text feild for search and inputs

--- lib/core/widgets/custome_text_form_field.dart
+++ lib/core/widgets/custome_text_form_field.dart
@@ -9,15 +9,15 @@ class CustomTextFormField extends StatefulWidget {
   final String hintText;
   final TextEditingController? controller;
   final void Function(String)? onChanged;
-  final String prefixIconName;
+  final String ?prefixIconName;
   final String? Function(String?)? validator;
   final void Function(String)? onSubmitted;
 
   final bool isPassword;
-  const CustomTextFormField({
+   CustomTextFormField({
     super.key,
     required this.hintText,
-    required this.prefixIconName,
+     this.prefixIconName,
     this.isPassword = false,
     this.controller,
     this.onChanged,
@@ -37,6 +37,7 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
       final settingsProvider = context.watch<SettingsProvider>();
 
     return TextFormField(
+      
       controller: widget.controller,
       onChanged: widget.onChanged,
       onFieldSubmitted: widget.onSubmitted,
@@ -45,18 +46,18 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
         fontSize: 16.sp,
         color:settingsProvider.isDark? AppTheme.white:AppTheme.gray,
       ),
-      decoration: InputDecoration(
+      decoration: widget.prefixIconName!=null?InputDecoration(
         prefixIcon: Padding(
           padding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 12.w),
           child: SvgPicture.asset(
-            widget.prefixIconName,
+            widget.prefixIconName!,
             width: 20.w,
             height: 20.h,
           ),
         ),
 
         hintText: widget.hintText,
-      ),
+      ):null,
       cursorColor: AppTheme.white,
       validator: widget.validator,
       obscureText: isObscure,
